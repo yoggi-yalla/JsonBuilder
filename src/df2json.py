7b34053fb83a0545c211c30fcb5a8d09fbceeae6
yoggi-yalla <oskar.p@live.se>
2019-05-20 08:28:49 +0200

    .

--- src/df2json.py
+++ src/df2json.py
@@ -8,15 +8,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-instructions', nargs='?')
 args = parser.parse_args()
 
-df = pd.read_excel('test.xlsx').astype(str)
+df = pd.read_excel('../test/test.xlsx').astype(str)
 
 def main():
 
     print(df)
     
-    # print(dataFrame.groupby(['name'], as_index=True).apply(lambda x: x.to_dict('r')).to_json())
+    print(df.groupby(['name'], as_index=True).apply(lambda x: x.to_dict('r')).to_json())
 
-    with open('test.json') as f:
+    with open('../test/test.json') as f:
         targetJson = json.load(f)
 
     print('\n' + 'target:')
