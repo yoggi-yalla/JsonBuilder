361ad73e5bebfd8e6f978a7f0919b637a6374258
yoggi-yalla <oskar.p@live.se>
2019-06-10 21:07:14 +0200

    .

--- src/df2json.py
+++ src/df2json.py
@@ -11,42 +11,44 @@ parser.add_argument('-mapping', nargs='?')
 args = parser.parse_args()
 
 
+
 '''
 item
-type
-shape
-name
-value
-display
-filter
-group
-func
+type        = mandatory for all items
+name        = mandatory for items within an object
+shape       = mandatory for objects and arrays
+value       = mandatory for simple
+display     = optional
+scope       = optional
+group       = optional
+func        = optional
 
 an item is the highest form of abstraction, each item has a type
 
-type can be object, array, or simple
-an object can hold name:item pairs
-an array can hold items
-a simple is the lowest form of data and can't hold another item
+type can be array, object, or simple
+an object can hold items with a name
+an array can hold items without name
+a simple can't hold another item
 
 shape contains all the items that are held by the parent item
 
-name is the name of an item within an object
+value is the value (string, float, int, true, false, or none) of a simple
 
-value is the value (string, float, int, bool, or none) of a simple
+display will be used in a GUI to help navigate/build the mapping
 
-display might get used in a GUI later on to help navigate/build the mapping
+scope will be used to set the scope of the dataframe
 
-filter is used to set the scope of the dataframe
+group will be used to split the scope into groups and build one item for each group
 
-group is used to split the scope into groups and build one item for each group
+(every row of the current scope is a group by default)
 
-func is used to apply a function to an item after it has been created
+func will be used to apply a function to an item after it has been created
 '''
 
 output = {}
+level = 0
 
-# mapping and data frame will be the user input 
+# mapping and data frame will be the input 
 mapping = {  
     "type":"object",
     "shape":[  
@@ -65,7 +67,7 @@ mapping = {
                         {  
                             "type":"simple",
                             "name":"name",
-                            "value":"EUR_OIS"
+                            "value":"USD_OIS"
                         },
                         {  
                             "type":"array",
@@ -167,22 +169,15 @@ def main():
     df = pd.read_csv('../test/test.txt', na_filter=False).astype(str)
     scope = df
 
-    print('\nFull dataframe:')
-    print(df)
-    print('')
-    
-    # df.loc is used to set the scope of the dataframe
-    print('Filtered for EUR_OIS:')
-    df = df.loc[df.name == 'EUR_OIS']
+    print('\n Full dataframe:')
     print(df)
     print('')
 
-    level = 0
     output = process_item(df, mapping, level)
     print(json.dumps(output, indent=4))
 
-def process_item(df, item, level):
-    
+def process_item(scope, item, level):
+
     if item["type"] == "simple":
         return item["value"]
 
