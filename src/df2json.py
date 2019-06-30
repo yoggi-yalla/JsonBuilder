4cf90af5e748afb070b89fb2dff496e407faf9ca
yoggi-yalla <oskar.p@live.se>
2019-06-30 22:12:21 +0200

    .

--- src/df2json.py
+++ src/df2json.py
@@ -1,49 +1,38 @@
 import pandas as pd
 import argparse
 import json
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-data', nargs='?')
 parser.add_argument('-mapping', nargs='?')
 args = parser.parse_args()
 
+df = pd.read_csv(args.data)
+mapping = json.load(open(args.mapping))
 
-# setup
-if args.data:
-    df = pd.read_csv(args.data)
-else:
-    df = pd.read_csv('../test/test.txt', na_filter=False).astype(str)
-
-if args.mapping:
-    with open(args.mapping, 'r') as f:
-        mapping = json.load(f)
-else:
-    with open('../test/mapping3.json', "r") as f:
-        mapping = json.load(f)
-
-assert mapping["type"] in ["array", "object"]
+assert mapping["type"] in ["object", "array"]
 assert mapping["group"] == True
-assert "scope" not in mapping
+assert "func" not in mapping
 
-memory = {}
-json_level = 0
+'''
+Test this script by running below line in the terminal:
+python df2json.py -data '../test/test.txt' -mapping '../test/mapping3.json'
+'''
 
 
 def main():
-
-    traverse(df, mapping)
-    output = memory[1]
-
-    print('\n Full dataframe:')
-    print(df)
-    print('')
-    print(json.dumps(output, indent=3))
-
-
-def traverse(df, node):
     
-    global json_level
-    global memory
+    memory = {}
+    level = 0
+
+    output = traverse(df, mapping, memory, level)
+
+    print(json.dumps(output, indent=3))
+    print(time.process_time())
+
+
+def traverse(df, node, memory, level):
 
     if "scope" in node:
         scope = df.loc[eval(node["scope"])]
@@ -56,51 +45,47 @@ def traverse(df, node):
         else:
             iterator = scope.groupby(node["group"])
     else:
-        iterator = scope.groupby(scope.index)
-
+        iterator = scope.groupby(scope.index)   
 
     for group in iterator:
-        
-        if "name_col" in node:
-            node["name"] = group[1].iloc[0][node["name_col"]]
 
+        level += 1
 
         if node["type"] == "leaf":
             if "value_col" in node:
-                child = group[1].iloc[0][node["value_col"]]
+                memory[level] = group[1].iloc[0][node["value_col"]]
             else:
-                child = node["value"]
+                memory[level] = node["value"]
 
         elif node["type"] == "object":
-            json_level += 1
-            memory[json_level] = {}
+            memory[level] = {}
             for sub_node in node["children"]:
-                traverse(group[1], sub_node)
-            json_level -= 1
-            child = memory[json_level+1]
+                traverse(group[1], sub_node, memory, level)
 
         elif node["type"] == "array":
-            json_level += 1
-            memory[json_level] = []
+            memory[level] = []
             for sub_node in node["children"]:
-                traverse(group[1], sub_node)
-            json_level -= 1
-            child = memory[json_level+1]
-
+                traverse(group[1], sub_node, memory, level)
+                
+        if "name_col" in node:
+            node["name"] = group[1].iloc[0][node["name_col"]]
 
         if "func" in node:
             f = eval(node["func"])
-            child = f(child, scope.iloc[0], memory[json_level])
+            memory[level] = f(memory[level], scope.iloc[0], memory[level-1])
 
+        level -= 1
 
-        if json_level == 0:
-            pass
-        else:
-            if type(memory[json_level]) == dict:
-                memory[json_level][node["name"]] = child
+        if level == 0:
+            return memory[1]
+        else:            
+            if type(memory[level]) == dict:
+                memory[level][node["name"]] = memory[level+1]
+                del memory[level+1]
             else:
-                memory[json_level].append(child)
-
+                memory[level].append(memory[level+1])
+                del memory[level+1]
+        
 
 if __name__ == "__main__":
     main()
