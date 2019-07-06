e9b3ef1994e378dcaa37baef61226a4578762503
yoggi-yalla <oskar.p@live.se>
2019-07-06 15:23:48 +0200

    .

--- src/df2json.py
+++ src/df2json.py
@@ -16,76 +16,131 @@ assert mapping["group"] == True
 assert "func" not in mapping
 
 '''
-Test this script by running below line in the terminal:
+Run the script in command line from this working dir:
 python df2json.py -data '../test/test.txt' -mapping '../test/mapping3.json'
+testhuge.txt is ~50 000 lines of csv and runs at ~5 sec
 '''
 
 
 def main():
-    
-    memory = {}
-    level = 0
 
-    output = traverse(df, mapping, memory, level)
+    '''
+    mem is used as RAM when building the json
+    ref is used to reference data in mem
+    '''
+    mem = {}
+    ref = 0
+
+    output = traverse(df, mapping, mem, ref)
 
     print(df)
     print(json.dumps(output, indent=3))
     print(time.process_time())
 
 
-def traverse(df, node, memory, level):
+def traverse(df, node, mem, ref):
 
-    if "scope" in node:
-        scope = df.loc[eval(node["scope"])]
+    if "filter" in node:
+        scope = df.loc[eval(node["filter"])]
     else:
         scope = df
 
     if "group" in node:
         if node["group"] == True:
-            iterator = scope.groupby(scope.index != None)
+            iterator = scope.groupby(scope.index != None, sort=False)
         else:
-            iterator = scope.groupby(node["group"])
+            iterator = scope.groupby(node["group"], sort=False)
     else:
-        iterator = scope.groupby(scope.index)   
+        iterator = scope.itertuples()
 
-    for group in iterator:
+    if type(iterator) == map:
+        for row in iterator:
+            process_row(row, node, mem, ref)
+    else:
+        for group in iterator:
+            process_group(group, node, mem, ref)
 
-        level += 1
+    return mem[1]
 
-        if node["type"] == "leaf":
-            if "value_col" in node:
-                memory[level] = group[1].iloc[0][node["value_col"]]
-            else:
-                memory[level] = node["value"]
+def process_row(row, node, mem, ref):
 
-        elif node["type"] == "object":
-            memory[level] = {}
-            for sub_node in node["children"]:
-                traverse(group[1], sub_node, memory, level)
+    ref += 1
 
-        elif node["type"] == "array":
-            memory[level] = []
-            for sub_node in node["children"]:
-                traverse(group[1], sub_node, memory, level)
-                
-        if "name_col" in node:
-            node["name"] = group[1].iloc[0][node["name_col"]]
+    if node["type"] == "object":
+        mem[ref] = {}
+        for child in node["children"]:     
+            process_row(row, child, mem, ref)
 
-        if "func" in node:
-            f = eval(node["func"])
-            memory[level] = f(memory[level], scope.iloc[0], memory[level-1])
+    elif node["type"] == "array":
+        mem[ref] = []
+        for child in node["children"]:
+            process_row(row, child, mem, ref)
 
-        level -= 1
+    elif node["type"] == "leaf":
+        if "value_col" in node:
+            mem[ref] = getattr(row, node["value_col"])
+        else:
+            mem[ref] = node["value"]
+            
+    if "name_col" in node:
+        node["name"] = getattr(row, node["name_col"])
 
-        if level == 0:
-            return memory[1]
-        else:            
-            if type(memory[level]) == dict:
-                memory[level][node["name"]] = memory[level+1]
-                del memory[level+1]
-            else:
-                memory[level].append(memory[level+1])
-                del memory[level+1]
+    if "func" in node:
+        f = eval(node["func"])
+        mem[ref] = f(mem[ref], row, mem[ref-1])
+
+    ref -= 1
+
+    if ref == 0:
+        pass
+    else:            
+        if type(mem[ref]) == dict:
+            mem[ref][node["name"]] = mem[ref+1]
+            del mem[ref+1]
+        else:
+            mem[ref].append(mem[ref+1])
+            del mem[ref+1]
+
+
+def process_group(group, node, mem, ref):
+    
+    ref += 1
+
+    if node["type"] == "object":
+        mem[ref] = {}
+        for child in node["children"]:
+            traverse(group[1], child, mem, ref)
+
+    elif node["type"] == "array":
+        mem[ref] = []
+        for child in node["children"]:
+            traverse(group[1], child, mem, ref)
+
+    elif node["type"] == "leaf":
+        if "value_col" in node:
+            mem[ref] = group[1].iloc[0][node["value_col"]]
+        else:
+            mem[ref] = node["value"]
+            
+    if "name_col" in node:
+        node["name"] = group[1].iloc[0][node["name_col"]]
+
+    if "func" in node:
+        f = eval(node["func"])
+        mem[ref] = f(mem[ref], group[1].iloc[0], mem[ref-1])
+
+    ref -= 1
+
+    if ref == 0:
+        pass
+    else:            
+        if type(mem[ref]) == dict:
+            mem[ref][node["name"]] = mem[ref+1]
+            del mem[ref+1]
+        else:
+            mem[ref].append(mem[ref+1])
+            del mem[ref+1]
+       
         
 
 if __name__ == "__main__":
