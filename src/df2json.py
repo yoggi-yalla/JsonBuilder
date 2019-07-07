d48a8a1f672bf3527ff6d9bb278999b05fcc5448
yoggi-yalla <oskar.p@live.se>
2019-07-07 14:32:36 +0200

    .

--- src/df2json.py
+++ src/df2json.py
@@ -12,13 +12,12 @@ df = pd.read_csv(args.data)
 mapping = json.load(open(args.mapping))
 
 assert mapping["type"] in ["object", "array"]
-assert mapping["group"] == True
 assert "func" not in mapping
+assert "multiple" not in mapping
 
 '''
 Run the script in command line from this working dir:
 python df2json.py -data '../test/test.txt' -mapping '../test/mapping3.json'
-testhuge.txt is ~50 000 lines of csv and runs at ~5 sec
 '''
 
 
@@ -34,7 +33,7 @@ def main():
     output = traverse(df, mapping, mem, ref)
 
     print(df)
-    print(json.dumps(output, indent=3))
+    print(json.dumps(output, indent=3, sort_keys=True))
     print(time.process_time())
 
 
@@ -46,19 +45,14 @@ def traverse(df, node, mem, ref):
         scope = df
 
     if "group" in node:
-        if node["group"] == True:
-            iterator = scope.groupby(scope.index != None, sort=False)
+        if node["group"] == False:
+            for row in scope.itertuples():
+                process_row(row, node, mem, ref)
         else:
-            iterator = scope.groupby(node["group"], sort=False)
+            for group in scope.groupby(node["group"], sort=False):
+                process_scope(group[1], node, mem, ref)
     else:
-        iterator = scope.itertuples()
-
-    if type(iterator) == map:
-        for row in iterator:
-            process_row(row, node, mem, ref)
-    else:
-        for group in iterator:
-            process_group(group, node, mem, ref)
+        process_scope(scope, node, mem, ref)
 
     return mem[1]
 
@@ -91,46 +85,42 @@ def process_row(row, node, mem, ref):
 
     ref -= 1
 
-    if ref == 0:
-        pass
-    else:            
-        if type(mem[ref]) == dict:
-            mem[ref][node["name"]] = mem[ref+1]
-            del mem[ref+1]
-        else:
-            mem[ref].append(mem[ref+1])
-            del mem[ref+1]
+    attach(node, mem, ref)
 
 
-def process_group(group, node, mem, ref):
-    
+def process_scope(group, node, mem, ref):
+
     ref += 1
 
     if node["type"] == "object":
         mem[ref] = {}
         for child in node["children"]:
-            traverse(group[1], child, mem, ref)
+            traverse(group, child, mem, ref)
 
     elif node["type"] == "array":
         mem[ref] = []
         for child in node["children"]:
-            traverse(group[1], child, mem, ref)
+            traverse(group, child, mem, ref)
 
     elif node["type"] == "leaf":
         if "value_col" in node:
-            mem[ref] = group[1].iloc[0][node["value_col"]]
+            mem[ref] = group.iloc[0][node["value_col"]]
         else:
             mem[ref] = node["value"]
             
     if "name_col" in node:
-        node["name"] = group[1].iloc[0][node["name_col"]]
+        node["name"] = group.iloc[0][node["name_col"]]
 
     if "func" in node:
         f = eval(node["func"])
-        mem[ref] = f(mem[ref], group[1].iloc[0], mem[ref-1])
+        mem[ref] = f(mem[ref], group.iloc[0], mem[ref-1])
 
     ref -= 1
 
+    attach(node, mem, ref)
+
+
+def attach(node, mem, ref):
     if ref == 0:
         pass
     else:            
