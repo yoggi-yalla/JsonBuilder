33c627e12afe3c60675df5da4e24c9bab592855d
yoggi-yalla <oskar.p@live.se>
2019-12-16 21:09:33 +0100

    haj

--- src/df2json.py
+++ src/df2json.py
@@ -11,26 +11,18 @@ args = parser.parse_args()
 df = pd.read_csv(args.data)
 mapping = json.load(open(args.mapping))
 
-assert mapping["type"] in ["object", "array"]
-assert "func" not in mapping
-assert "split" not in mapping
-
 '''
 Run the script in command line from this working dir:
 python df2json.py -data ../test/test.txt -mapping ../test/mapping3.json
 '''
 
-
 def main():
-
-    '''
-    mem is used as RAM when building the json
-    ref is used to reference data in mem
-    '''
+    
     mem = {}
     ref = 0
+    tree = build_tree(mapping)
 
-    output = traverse(df, mapping, mem, ref)
+    output = traverse(df, tree, mem, ref)
 
     print(df)
     out = json.dumps(output, indent=3, sort_keys=True, ensure_ascii=False)
@@ -39,88 +31,77 @@ def main():
 
 
 def traverse(df, node, mem, ref):
+    if node.filter:
+        df = df.loc[eval(node.filter)]
 
-    if "filter" in node:
-        df = df["EUR" in df.name.asobject]
-        #df = df[eval(node["filter"])]
-        #df = df.loc[eval(node["filter"])]
-
-    if "multiple" in node and node["multiple"] == True:
-
-        if "group_by" in node:
-            for group in df.groupby(eval(node["group_by"]), sort=False):
+    if node.multiple == True:
+        if node.group_by:
+            for group in df.groupby(eval(node.group_by), sort=False):
                 process_group(group[1], node, mem, ref)
-
         else:
-            for row in df.itertuples():
+            for row in df.itertuples():          
                 process_row(row, node, mem, ref)
     else:
         process_group(df, node, mem, ref)
     
     return mem[1]
 
-
 def process_group(df, node, mem, ref):
-
     ref += 1
 
-    if node["type"] == "object":
+    if node.type == "object":
         mem[ref] = {}
-        for child in node["children"]:
+        for child in node.children:
             traverse(df, child, mem, ref)
 
-    elif node["type"] == "array":
+    elif node.type == "array":
         mem[ref] = []
-        for child in node["children"]:
+        for child in node.children:
             traverse(df, child, mem, ref)
 
-    elif node["type"] == "leaf":
-        if "value_col" in node:
-            mem[ref] = df.iloc[0][node["value_col"]]
+    elif node.type == "leaf":
+        if node.value_col:
+            mem[ref] = df.iloc[0][node.value_col]
         else:
-            mem[ref] = node["value"]
+            mem[ref] = node.value
             
-    if "name_col" in node:
-        node["name"] = df.iloc[0][node["name_col"]]
+    if node.name_col:
+        node.name = df.iloc[0][node.name_col]
 
-    if "func" in node:
-        f = eval(node["func"])
+    if node.func:
+        f = eval(node.func)
         mem[ref] = f(mem[ref], df.iloc[0], mem[ref-1])
 
     ref -= 1
-
     attach(node, mem, ref)
 
-
 def process_row(row, node, mem, ref):
-
     ref += 1
 
-    if node["type"] == "object":
+    if node.type == "object":
         mem[ref] = {}
-        for child in node["children"]:     
+        for child in node.children:     
             process_row(row, child, mem, ref)
 
-    elif node["type"] == "array":
+    elif node.type == "array":
         mem[ref] = []
-        for child in node["children"]:
+        for child in node.children:
             process_row(row, child, mem, ref)
 
-    elif node["type"] == "leaf":
-        if "value_col" in node:
-            mem[ref] = getattr(row, node["value_col"])
+    elif node.type == "leaf":
+        if node.value_col:
+            mem[ref] = getattr(row, node.value_col)
         else:
-            mem[ref] = node["value"]
+            mem[ref] = node.value
             
-    if "name_col" in node:
-        node["name"] = getattr(row, node["name_col"])
+    if node.name_col:
+        node.name = getattr(row, node.name_col)
 
-    if "func" in node:
-        f = eval(node["func"])
+    if node.func:
+        f = eval(node.func)
         mem[ref] = f(mem[ref], row, mem[ref-1])
 
     ref -= 1
-
     attach(node, mem, ref)
 
 
@@ -129,12 +110,43 @@ def attach(node, mem, ref):
         pass
     else:            
         if type(mem[ref]) == dict:
-            mem[ref][node["name"]] = mem[ref+1]
+            mem[ref][node.name] = mem[ref+1]
             del mem[ref+1]
         else:
             mem[ref].append(mem[ref+1])
             del mem[ref+1]
-       
+
+
+class Node(object):
+    def __init__(self, data):
+        self.type = data["type"]
+        self.name = data.get("name")
+        self.name_col = data.get("name_col")
+        self.value = data.get("value")
+        self.value_col = data.get("value_col")
+        self.filter = data.get("filter")
+        self.multiple = data.get("multiple")
+        self.group_by = data.get("group_by")
+        self.func = data.get("func")
+        self.children = []
+
+    def add_child(self, obj):
+        self.children.append(obj)
+
+def build_tree(mapping):
+    root = Node(mapping)
+    for c in mapping["children"]:
+        build_sub_tree(root, c)
+    return root
+
+def build_sub_tree(parent, mapping):
+    this_node = Node(mapping)
+    parent.add_child(this_node)
+    if mapping.get("children") == None or len(mapping["children"]) == 0:
+        pass
+    else:
+        for c in mapping["children"]:
+            build_sub_tree(this_node, c)
         
 
 if __name__ == "__main__":
