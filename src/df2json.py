89e209eccc6d27a168e1bce77616289e1e4d321e
yoggi-yalla <oskar.p@live.se>
2020-04-12 09:59:32 +0200

    Signed-off-by: yoggi-yalla <oskar.p@live.se>

--- src/df2json.py
+++ src/df2json.py
@@ -7,40 +7,42 @@ import cProfile
 import pstats
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-t', '--table')
 parser.add_argument('-f', '--format')
+parser.add_argument('-t', '--table', nargs ='?')
 parser.add_argument('-o', '--output', nargs='?')
 args = parser.parse_args()
 
 
 '''
 Run the script in command line from this working dir:
-python df2json.py -t ../test/test.txt -f ../test/format3.json
+python df2json.py -t ../test/test.csv -f ../test/format3.json
 '''
-
 
 
 with open(args.format) as f:
     format = json.load(f)
 
+mapping = format.get("mapping", {})
 
+if args.table:
+    df = pd.read_csv(args.table)
+else:
+    df = pd.DataFrame()
+
+for t in format.get("df_transforms",[]):
+    exec(t)
+    
 for f in format.get("functions",[]):
     exec(f)
 
-
-df = pd.read_csv(args.table)
-for t in format.get("df_transforms",[]):
-    exec(t)
-
-mem = []
+mem = [] # dedicated stack memory for json construction
 
 
-def main():
+def main(): 
+    root = build_tree(mapping)
     
-    root = build_tree(format["mapping"])
-    
-    output_json = traverse(root, df)
-    output_str = rapidjson.dumps(output_json, indent=4)
+    output_raw = traverse(root, df)
+    output_str = rapidjson.dumps(output_raw, indent=3)
     
     if args.output:
         with open(args.output, "w") as f:
@@ -52,7 +54,6 @@ def main():
 
 
 def traverse(node, df):
-
     if node.filter:
         df = df[eval(node.filter)]
 
@@ -65,52 +66,35 @@ def traverse(node, df):
                 row = row._asdict()
                 process(node, row=row)
     else:
-        if len(df) > 0:
-            process(node, df=df)
+        process(node, df=df)
     
     return mem[-1] if mem else None
 
 
-def process(node, df=None, row=None):
-
-    if row is None:
+def process(node, df=pd.DataFrame(), row={}):
+    if not row and len(df)>0:
         row = df.iloc[0].to_dict()
 
-    if node.type == "object":
-        mem.append({})
-        if df is None:
-            for child in node.children:
-                process(child, row=row)
-        else:
+    if node.type in ("object", "array"):
+        mem.append({} if node.type == "object" else [])
+        if len(df)>0:
             for child in node.children:
                 traverse(child, df)
-
-    elif node.type == "array":
-        mem.append([])
-
-        if df is None:
-            for child in node.children:
-                process(child, row=row)
         else:
             for child in node.children:
-                traverse(child, df)
-
-    elif node.type == "leaf":
+                process(child, row=row)
+    else:
         if node.value_col:
             mem.append(row.get(node.value_col))
         else:
             mem.append(node.value)
-
+    
     if node.name_col:
         node.name = row.get(node.name_col)
 
     if node.func:
-        mem[-1] = node.func(mem[-1], row, mem[-2])
+        mem[-1] = node.func(mem[-1], row, df)
 
-    attach(node, mem)
-
-
-def attach(node, mem):
     if len(mem) > 1:
         attachment = mem.pop()
         if isinstance(mem[-1], dict):
@@ -122,7 +106,7 @@ def attach(node, mem):
 class Node(object):
     def __init__(self, m):
         self.type = m.get("type", "leaf")
-        self.name = m.get("name")
+        self.name = m.get("name", "MISSING_NAME")
         self.name_col = m.get("name_col")
         self.value = m.get("value")
         self.value_col = m.get("value_col")
@@ -135,24 +119,21 @@ class Node(object):
     def add_child(self, obj):
         self.children.append(obj)
 
-
 def build_tree(m):
     root = Node(m)
     for c in m.get("children", []):
         build_sub_tree(root, c)
     return root
 
-
 def build_sub_tree(parent, m):
     this_node = Node(m)
     parent.add_child(this_node)
     for c in m.get("children", []):
         build_sub_tree(this_node, c)
-        
 
-
+    
 if __name__ == "__main__":
     main()
     #cProfile.run("main()", 'restats')
     #p = pstats.Stats('restats')
-    #p.sort_stats('cumtime').print_stats(35)
+    #p.sort_stats('time').print_stats(35)
