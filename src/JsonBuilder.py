8cfbff5a7006833dfad02bd345f21cf3d161ecd3
yoggi-yalla <oskar.p@live.se>
2020-04-26 08:47:00 +0200

    Signed-off-by: yoggi-yalla <oskar.p@live.se>

--- src/JsonBuilder.py
+++ src/JsonBuilder.py
@@ -1,69 +1,56 @@
-class JsonBuilder(object):
-    def __init__(self, m={}, df=None, functions={}):
-        self.type = m.get('type')
-        self.value_col = m.get('value_col')
-        self.name_col = m.get('name_col')
-        self.value = m.get('value')
-        self.name = m.get('name')
-        self.multiple = m.get('multiple')
-        self.children = []
+import pandas as pd
 
-        self.df = df
+class JsonBuilder:
+    def __init__(self, **kwargs):
+        self.type = kwargs.get('type')
+        self.value_col = kwargs.get('value_col')
+        self.name_col = kwargs.get('name_col')
+        self.value = kwargs.get('value')
+        self.name = kwargs.get('name')
+        self.multiple = kwargs.get('multiple')
+
+        self.func_raw = kwargs.get('func', 'None')
+        self.filter_raw = kwargs.get('filter', 'None')
+        self.group_by_raw = kwargs.get('group_by', 'None')
+
+        self.func = eval(self.func_raw)
+        self.filter = eval(self.filter_raw)
+        self.group_by = eval(self.group_by_raw)
+
+        self.df = None
         self.row = None
-        
-        globals().update(functions)
-        self.func = eval(m.get('func', 'None'))
-        self.filter = eval(m.get('filter', 'None'))
-        self.group_by = eval(m.get('group_by', 'None'))
 
-        for child in m.get('children', []):
-            self.children.append(JsonBuilder(child))
-        
-        if self.df is not None:
-            if self.filter:
-                self.df = self.df[self.filter(self.df)]
-            if len(self.df.index)>0:
-                self.row = next(df.itertuples())
+        self.children = []
 
     def build(self):
         if self.type == 'object':
             self._build_object()
-
         elif self.type == 'array':
             self._build_array()
-
         else:
             self._fetch_value()
-            
         self._fetch_name()
-
         self._apply_func()
-
-        return self.name, self.value
+        return self
 
     def _build_object(self):
         self.value = {}
         for child in self.children:
-                child.df, child.row = self.df, self.row
-                for _ in child._next_scope():
-                    k,v = child.build()
-                    self.value[k] = v
+            child.df, child.row = self.df, self.row
+            for _ in child._next_scope():
+                c = child.build()
+                self.value[c.name] = c.value
 
     def _build_array(self):
-        if self.value_col:
-            self.value = self.df[self.value_col].tolist()
-        else:
-            self.value = []
-            for child in self.children:
-                child.df, child.row = self.df, self.row
-                for _ in child._next_scope():
-                    k,v = child.build()
-                    self.value.append(v)
+        self.value = []
+        for child in self.children:
+            child.df, child.row = self.df, self.row
+            for _ in child._next_scope():
+                c = child.build()
+                self.value.append(c.value)
 
     def _next_scope(self):
-        if self.filter:
-            self.df = self.df[self.filter(self.df)]
-
+        self._apply_filter()
         if self.multiple:
             if self.group_by:
                 for group in self.df.groupby(self.group_by(self.df), sort=False):
@@ -82,9 +69,55 @@ class JsonBuilder(object):
             self.value = getattr(self.row, self.value_col)
     
     def _fetch_name(self):
-        if self.name_col and self.row is not None:
+        if self.name is None and self.name_col is None:
+            self.name = self.value_col
+        elif self.name_col and self.row is not None:
             self.name = getattr(self.row, self.name_col)
+
+    def _apply_filter(self):
+        if self.filter and self.df is not None:
+            self.df = self.df[self.filter(self.df)]
+            if len(self.df.index)>0:
+                self.row = next(self.df.itertuples())
+            else:
+                self.row = None
 
     def _apply_func(self):
         if self.func:
             self.value = self.func(self.value, self.row, self.df)
+
+
+    def parse_mapping(self, mapping):
+        children = mapping.pop('children', [])
+        self = JsonBuilder(**mapping)
+        for c in children:
+            self.children.append(JsonBuilder().parse_mapping(c))
+        return self
+
+    def load_csv(self, csv):
+        self.df = pd.read_csv(csv)
+        self.df.index += 1
+        self._apply_filter()
+        if self.df is not None:
+            self.row = next(self.df.itertuples())
+        print("DataFrame after loading: \n {} \n".format(self.df))
+        return self
+
+    def add_functions(self, functions):
+        new_functions = {}
+        for f in functions:
+            exec(f)
+            f_name = f.split('(')[0].split(' ')[1]
+            new_functions[f_name] = eval(f_name)
+        globals().update(new_functions)
+        return self
+    
+    def apply_transforms(self, df_transforms):
+        if df_transforms:
+            for transform in df_transforms:
+                f = eval(transform)
+                col = f(self.df)
+                name = col.name
+                self.df[name] = col
+            print("DataFrame after transforms: \n {} \n".format(self.df))
+        return self
---
+++ src/csv2json.py
@@ -0,0 +1,57 @@
+from JsonBuilder import JsonBuilder
+import argparse
+import time
+import json
+import orjson
+
+import cProfile
+import pstats
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--format')
+parser.add_argument('-t', '--table')
+parser.add_argument('-o', '--output')
+parser.add_argument('-s', '--silent', dest='silent', action='store_true')
+args = parser.parse_args()
+
+'''
+Run the script in command line from this working dir:
+python csv2json.py -t ../testdata/test.csv -f ../test/format3.json
+'''
+
+def main(): 
+    with open(args.format) as f:
+        format = json.load(f)
+
+    functions = format.get('functions',[])
+    transforms = format.get('df_transforms', [])
+    mapping = format.get('mapping')
+
+    output_native = JsonBuilder().parse_mapping(mapping)          \
+                                 .add_functions(functions)        \
+                                 .load_csv(args.table)            \
+                                 .apply_transforms(transforms)    \
+                                 .build()                         \
+                                 .value
+
+
+    output_binary = orjson.dumps(output_native,
+                                 option=orjson.OPT_INDENT_2|
+                                 orjson.OPT_NON_STR_KEYS,
+                                 default=lambda x:None)
+
+    if not args.silent:
+        if args.output:
+            with open(args.output, 'wb') as f:
+                f.write(output_binary)
+        else:
+            print(output_binary.decode('UTF8'))
+
+    print(time.process_time())
+
+if __name__ == '__main__':
+    main()
+    #cProfile.run('main()', 'tmp')
+    #pstats.Stats('tmp').sort_stats('time').print_stats(20)
+    #os.remove('tmp')
